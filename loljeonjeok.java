f198bcecb01c252f0fcf1e83decaf7e8cf3a29a7
ksooo091 <78585294+ksooo091@users.noreply.github.com>
2021-02-17 17:22:21 +0900

    Add files via upload

--- loljeonjeok.java
+++ loljeonjeok.java
@@ -14,6 +14,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+// integer.paser ~~ 는 값들이 다 오브젝트형식이라 사용이 안되서 했씁니다. 혹시나 변환 이렇게 안하고 사용하실수 있는법을 아신다면 알려주세요! 
+
 public class loljeonjeok {
 	HttpsURLConnection huc = null;
 	String api = "RGAPI-a531df28-32d7-4b3e-bbf7-78897704cbd4";
@@ -25,23 +27,17 @@ public class loljeonjeok {
 			sname = URLEncoder.encode(sname, "utf-8");
 			String url = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
 			url = url + sname + "?api_key=" + api;
-
 			URL u = new URL(url);
 			huc = (HttpsURLConnection) u.openConnection();
-//	huc.addRequestProperty("X-Riot-Token", api);
 			InputStream is = huc.getInputStream();
 			InputStreamReader isr = new InputStreamReader(is, "utf-8");
 
 			JSONParser jp = new JSONParser();
 			JSONObject loldata = (JSONObject) jp.parse(isr);
-			// 데이터 요청
-			// System.out.println(loldata);
-
-//	JSONObject sumonnerdata =(JSONObject) loldata.get(0);
-
 			System.out.println(loldata.get("name"));
 			System.out.println("어카운트id " + loldata.get("accountId"));
 			System.out.println("소환사 레벨 " + loldata.get("summonerLevel"));
+			
 			return (String) loldata.get("accountId");
 
 		} catch (Exception e) {
@@ -57,39 +53,30 @@ public class loljeonjeok {
 			String url = "https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/";
 			String sid = idck();
 			url = url + sid + "?api_key=" + api;
-
 			URL u = new URL(url);
 			huc = (HttpsURLConnection) u.openConnection();
-//			huc.addRequestProperty("X-Riot-Token", api);
 			InputStream is = huc.getInputStream();
 			InputStreamReader isr = new InputStreamReader(is, "utf-8");
-
+			
 			JSONParser jp = new JSONParser();
 			JSONObject loldata = (JSONObject) jp.parse(isr);
-			System.out.println(loldata);
-			System.out.println("1");
 			JSONArray jjs = (JSONArray) loldata.get("matches");
-			System.out.println(loldata.get("matches"));
 			JSONObject matches = (JSONObject) jjs.get(0);
-			System.out.println(matches.get("gameId"));
+			
 			for (int i = 0; i < 11; i++) {
-
 				JSONObject jj = (JSONObject) jjs.get(i);
 				System.out.println("게임번호 " + jj.get("gameId"));
 				System.out.println("게임 타입 " + queuetype.queue(Integer.parseInt(jj.get("queue").toString())));
 				// System.out.println("사용한 챔피언 " + jj.get("champion"));
 				// int champkey = Integer.parseInt(jj.get("champion").toString());
 				System.out.println("사용한 챔피언 " + champ.champnameKr(Integer.parseInt(jj.get("champion").toString())));
-
 				Date timestamp = new Date((long) jj.get("timestamp"));
 				System.out.println(timestamp);
 				// System.out.println("타임스탬프 " + jj.get("timestamp"));
 
 				System.out.println("=====================");
 			}
-			// 1612965618237 1612967175349
-			return matches.get("gameId");
-			// System.out.println("34");
+			return matches.get("gameId"); // 제일 최근에 한 게임 아이디 리턴
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println("출력오류ㅠ");
@@ -112,54 +99,46 @@ public class loljeonjeok {
 			JSONParser jp = new JSONParser();
 			JSONObject loldata = (JSONObject) jp.parse(isr);
 			System.out.println("게임타입 : " + queuetype.queue(Integer.parseInt(loldata.get("queueId").toString())));
-			JSONArray participantIdentities = (JSONArray) loldata.get("participantIdentities");
-			JSONArray participants = (JSONArray) loldata.get("participants");
-			JSONArray teams = (JSONArray) loldata.get("teams");
-//	JSONArray player = (JSONArray) participantIdentities.get("player");
-			JSONObject blueteaminfo = (JSONObject) teams.get(0);
-			JSONObject purpleteaminfo = (JSONObject) teams.get(1);
-if (Integer.parseInt(loldata.get("queueId").toString()) == 420) {
-	JSONArray bluebans = (JSONArray) blueteaminfo.get("bans");
-	JSONArray purplebans = (JSONArray) purpleteaminfo.get("bans");
-	System.out.println("블루팀 밴");
-	for (int i = 0; i < bluebans.size(); i++) {
-		JSONObject blueban =  (JSONObject) bluebans.get(i);
-		System.out.println(champ.champnameKr(Integer.parseInt(blueban.get("championId").toString())));
-	}
-	System.out.println("퍼플팀 밴");
-	for (int i = 0; i < purplebans.size(); i++) {
-		JSONObject purpleban =  (JSONObject) purplebans.get(i);
-		System.out.println(champ.champnameKr(Integer.parseInt(purpleban.get("championId").toString())));
-	}
-
-
-
-}			
-
 			
+			JSONArray teams = (JSONArray) loldata.get("teams"); 
+			JSONObject blueteaminfo = (JSONObject) teams.get(0); // 블루팀 데이터
+			JSONObject purpleteaminfo = (JSONObject) teams.get(1); // 레드팀 데이터
+			
+			if (Integer.parseInt(loldata.get("queueId").toString()) == 420) { // 솔로랭크 일때만 밴 목록 출력
+				JSONArray bluebans = (JSONArray) blueteaminfo.get("bans");
+				JSONArray purplebans = (JSONArray) purpleteaminfo.get("bans");
+				System.out.println("블루팀 밴 : ");
+				for (int i = 0; i < bluebans.size(); i++) {
+					JSONObject blueban = (JSONObject) bluebans.get(i);
+					System.out.println(champ.champnameKr(Integer.parseInt(blueban.get("championId").toString())));
+				}
+				System.out.println("퍼플팀 밴 : ");
+				for (int i = 0; i < purplebans.size(); i++) {
+					JSONObject purpleban = (JSONObject) purplebans.get(i);
+					System.out.println(champ.champnameKr(Integer.parseInt(purpleban.get("championId").toString())));
+				}
 
-//			if (blueteaminfo.get("win").equals("Win")) {
-//				bluer = "Win";
-//				purr = "Lose";
-//			}
-//			if (purpleteaminfo.get("win").equals("Win")) {
-//				bluer = "Lose";
-//				purr = "Win";
-//			}
+			}
+
+
 			System.out.println("=====================");
+			JSONArray participantIdentities = (JSONArray) loldata.get("participantIdentities");
+			JSONArray participants = (JSONArray) loldata.get("participants"); // 구성원 데이터
 
 			for (int i = 0; i < participantIdentities.size(); i++) {
-				JSONObject participant1 = (JSONObject) participants.get(i);
+				JSONObject participant1 = (JSONObject) participants.get(i); // 인게임 정보
 				JSONObject stats = (JSONObject) participant1.get("stats");
-				JSONObject participant2 = (JSONObject) participantIdentities.get(i);
+				JSONObject participant2 = (JSONObject) participantIdentities.get(i); // 인게임 외적 정보
 				JSONObject player = (JSONObject) participant2.get("player");
 				int pId = Integer.parseInt(participant2.get("participantId").toString());
-				System.out.println("사용자번호 " + pId);
+				System.out.println("게임 내 구성원 번호 " + pId);
+				
 				if ((boolean) stats.get("win")) {
 					System.out.println("게임 결과 : 승리 ");
 				} else {
 					System.out.println("게임 결과 : 패배 ");
 				}
+				
 				System.out.println("소환사명 " + player.get("summonerName"));
 				System.out.println(
 						"플레이한 챔피언 :" + champ.champnameKr(Integer.parseInt(participant1.get("championId").toString())));
